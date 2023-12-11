50a7a5a38ff71a41cdbb2bc952f3a6eb17c64047
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2023-12-10 21:01:32 -0800

    Add files via upload

---
+++ ArmstrongNo/ArmstrongNo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Armstrong
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Armstrong numbers between 1 and 1000 are:");
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (IsArmstrong(i))
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+    static bool IsArmstrong(int number)
+    {
+        int originalNumber = number;
+        int remainder, result = 0;
+        int n = originalNumber.ToString().Length;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += (int)Math.Pow(remainder, n);
+            originalNumber /= 10;
+        }
+
+        return result == number;
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class SimpleCalculator{
+    static void Main(){
+        int res =0;
+        Console.WriteLine("Enter the expressio:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        string op = Console.ReadLine();
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        switch (op){
+            case "+": res = a + b; break;
+            case "-": res = a - b; break;
+            case "*": res = a * b; break;
+            case "/": 
+                if (b == 0)
+                {
+                    Console.WriteLine("Cannot divide by 0...");
+                    
+                }
+                else
+                {
+                    res = a / b; break;
+                } break;
+            case "%":
+                if (b != 0)
+                {
+                    res = a % b; break;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot divide by 0...");
+                }break;
+            default: Console.WriteLine("invalid expression..");break;
+        }
+        Console.WriteLine("result: " + res);
+    }
+}
+
